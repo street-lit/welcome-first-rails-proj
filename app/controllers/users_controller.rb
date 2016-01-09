d4b2c9425066a8f49ca549e49542b181b227c068
MikeRobertHughes <hughes.mike.r@gmail.com>
2016-01-09 14:36:07 -0600

    Add params information to different actions and allow user to be created via url request (complete nightmare mode)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,12 @@ class UsersController < ApplicationController
       users_list = User.where("first_name LIKE ?", "#{params[:first_name]}%")
       users_list = users_list.map { |user| "#{user.full_name}, #{user.age}" }
       users_formatted = users_list.join(" ")
-      render({text: "Hello, your search returned: #{users_formatted} for users who's #{params.keys[0]} starts with #{params[:first_name]} ", status: 200})
+      render({text: "params.inspect: #{params.inspect} <br> params.size: #{params.size} <br> Hello, your search returned: #{users_formatted} for users who's #{params.keys[0]} starts with #{params[:first_name]} ", status: 200})
     elsif params.size > 2 && params.has_key?(:limit) && params.has_key?(:offset)
-      users_list = User.limit(10).offset(10)
-      users_list = users_list.map { |user| "id-#{user.id}:  #{user.full_name}, #{user.age} " }
+      users_list = User.limit(params[:limit]).offset(params[:offset])
+      users_list = users_list.map { |user| "id-#{user.id}:  #{user.full_name}, #{user.age} <br>" }
       users_formatted = users_list.join(" ")
-      render({text: "Hello, your search for a limit of 10 people, offset by 10 returned the following: #{users_formatted}", status: 200})
+      render({text: "params.inspect: #{params.inspect} <br> params.size: #{params.size} <br> Hello, your search for a limit of 10 people, offset by 10 returned the following: <br><br> #{users_formatted}", status: 200})
     end
   end
 
@@ -29,7 +29,17 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     render text: "user: #{user.full_name}, Age: #{user.age}"
   rescue ActiveRecord::RecordNotFound
-    render text: "Sorry, user with id of #{params[:id]} was not found", status: 404
+    render text: "params.inspect: #{params.inspect} <br> params.size: #{params.size} <br> Sorry, user with id of #{params[:id]} was not found", status: 404
+  end
+
+  def create
+    user = User.new
+    user.first_name = params[:first_name]
+    user.last_name = params[:last_name]
+    user.age = params[:age]
+    if user.save
+      render text: "params.inspect: #{params.inspect} <br> params.size: #{params.size} <br>  Hello, you created the user: #{params[:first_name]} #{params[:last_name]} with age #{params[:age]} and id #{user.id}", status: 200
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/users/create', to: 'users#create'
   get('/users', { to: 'users#index' })
   get('/users/:id', { to: 'users#show' })
   get('/users/:id/delete', { to: 'users#destroy' })
